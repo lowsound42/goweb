e35a0328a6c673a1748eebc361200be81dfcf62c
Omar Khan <o.khandxb@gmail.com>
2025-01-01 15:27:16 -0500

    routing started

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lowsound42/goweb
+
+go 1.23.4
--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:o.khandxb@gmail.com\">o.khandxb@gmail.com</a>.</p>")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Welcome to my space</h1>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, r.URL.Path)
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/path", pathHandler)
 	fmt.Println("Ahoy matey, we be sailin' on port :3000")
 	http.ListenAndServe(":3000", nil)
 }
