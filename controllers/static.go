2dd01b387d7678ce58da081086f777e6abd98348
Omar Khan <o.khandxb@gmail.com>
2025-01-02 18:34:48 -0500

    faq fix

--- controllers/static.go
+++ controllers/static.go
@@ -11,16 +11,16 @@ func FAQ(tpl Executor) http.HandlerFunc {
 		Answer   template.HTML
 	}{
 		{
-			Question: "Is there a free version?",
-			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+			Question: "Do you do business?",
+			Answer:   "Yes! Business is done here.",
 		},
 		{
-			Question: "What are your support hours?",
-			Answer:   "We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
+			Question: "What about customer service?",
+			Answer:   "So much of it.",
 		},
 		{
-			Question: "How do I contact support?",
-			Answer:   `Email us - <a href="mailto:o.khandxb@gmail.com">o.khandxb@gmail.com</a>`,
+			Question: "Give me your email address",
+			Answer:   `okay fine, <a href="mailto:o.khandxb@gmail.com">o.khandxb@gmail.com</a>`,
 		},
 	}
 	return func(w http.ResponseWriter, r *http.Request) {
