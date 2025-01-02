5ff611e4ef8b21ee40889a738c1bcd266570f8f2
Omar Khan <o.khandxb@gmail.com>
2025-01-02 17:32:55 -0500

    working on something weird

--- main.go
+++ main.go
@@ -15,9 +15,15 @@ func main() {
 
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.tmpl", "tailwind.tmpl"))))
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.tmpl", "tailwind.tmpl"))))
+	r.Get("/signup", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "signup.tmpl", "tailwind.tmpl")),
+	))
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS, "faq.tmpl", "tailwind.tmpl"))))
 
+	var usersC controllers.Users
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.tmpl", "tailwind.tmpl"))
+	r.Get("/signup", usersC.New)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
