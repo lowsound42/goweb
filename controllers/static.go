7735c6541158b295655a9b1b82b724afc8bd4d81
Omar Khan <o.khandxb@gmail.com>
2025-01-01 20:24:35 -0500

    dynamic templating

--- controllers/static.go
+++ controllers/static.go
@@ -1,10 +1,34 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/lowsound42/goweb/views"
 )
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:o.khandxb@gmail.com">o.khandxb@gmail.com</a>`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+}
 
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
--- views/template.go
+++ views/template.go
@@ -19,8 +19,8 @@ type Template struct {
 	htmlTpl *template.Template
 }
 
-func ParseFS(fs fs.FS, pattern string) (Template, error) {
-	htmlTpl, err := template.ParseFS(fs, pattern)
+func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
+	htmlTpl, err := template.ParseFS(fs, pattern...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
@@ -29,20 +29,9 @@ func ParseFS(fs fs.FS, pattern string) (Template, error) {
 	}, nil
 }
 
-func Parse(filepath string) (Template, error) {
-	tpl, err := template.ParseFiles(filepath)
-	if err != nil {
-		return Template{}, fmt.Errorf("parsing template: %w", err)
-	}
-
-	return Template{
-		htmlTpl: tpl,
-	}, nil
-}
-
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err := t.htmlTpl.Execute(w, nil)
+	err := t.htmlTpl.Execute(w, data)
 	if err != nil {
 		log.Printf("parsing template %v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
