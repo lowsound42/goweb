b2b476fd88352f603fcd9b641779fddac65aa5d9
Omar Khan <o.khandxb@gmail.com>
2025-01-05 08:05:04 -0500

    session clean up

--- models/session.go
+++ models/session.go
@@ -48,10 +48,24 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		TokenHash: ss.hash(token),
 	}
 	row := ss.DB.QueryRow(`
-		INSERT INTO sessions (user_id, token_hash)
-		VALUES ($1, $2)
-		RETURNING id;`, session.UserID, session.TokenHash)
+		UPDATE sessions
+		SET token_hash = $2
+		WHERE user_id = $1
+    RETURNING id;`, session.UserID, session.TokenHash)
 	err = row.Scan(&session.ID)
+	if err == sql.ErrNoRows {
+		// If no session exists, we will get ErrNoRows. That means we need to
+		// create a session object for that user.
+		row = ss.DB.QueryRow(`
+			INSERT INTO sessions (user_id, token_hash)
+			VALUES ($1, $2)
+			RETURNING id;`, session.UserID, session.TokenHash)
+		// The error will be overwritten with either a new error, or nil
+		err = row.Scan(&session.ID)
+	}
+	// If the err was not sql.ErrNoRows, we need to check to see if it was any
+	// other error. If it was sql.ErrNoRows it will be overwritten inside the if
+	// block, and we still need to check for any errors.
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
@@ -59,8 +73,24 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
-	// TODO: Implement SessionService.User
-	return nil, nil
+	tokenHash := ss.hash(token)
+	var user User
+	row := ss.DB.QueryRow(`
+		SELECT user_id
+		FROM sessions
+		WHERE token_hash = $1;`, tokenHash)
+	err := row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	row = ss.DB.QueryRow(`
+		SELECT email, password_hash
+		FROM users WHERE id = $1;`, user.ID)
+	err = row.Scan(&user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	return &user, nil
 }
 
 func (ss *SessionService) hash(token string) string {
